09d5f38e7909d6ef6c87424f476afcee166b08b8
Hailey Luong <luong.hailey@gmail.com>
2022-02-03 02:26:54 -0500

    Minor updates

--- js/script.js
+++ js/script.js
@@ -37,6 +37,48 @@ function hasScrolled() {
     lastScrollTop = st;
 }
 
+// from https://www.tutorialspoint.com/typing-and-deleting-effect-with-javascript-and-css
+
+const words = ["solving problems", "discovering music", "creating with Figma", "animals", "meeting other creatives"];
+let i = 0;
+let counter;
+function typeNow() {
+   let word = words[i].split("");
+   var loopTyping = function() {
+      if (word.length > 0) {
+         document.getElementById('shuffle').innerHTML += word.shift();
+      } else {
+         deleteNow();
+         return false;
+      };
+      counter = setTimeout(loopTyping, 125);
+   };
+   loopTyping();
+};
+function deleteNow() {
+   let word = words[i].split("");
+   var loopDeleting = function() {
+      if (word.length > 0) {
+         word.pop();
+         document.getElementById('shuffle').innerHTML = word.join("");
+      } else {
+         if (words.length > (i + 1)) {
+            i++;
+         }
+         else {
+            i = 0;
+         };
+         typeNow();
+         return false;
+      };
+      counter = setTimeout(loopDeleting, 75);
+   };
+   loopDeleting();
+};
+typeNow();
+
+//
+
 // cursor
 
 const cursor = document.querySelector('.cursor');
